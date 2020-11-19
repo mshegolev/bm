a55678ea5703d80bf3d82a53648bfe1c9ecbfb7a
Щеголев Михаил Владимирович <Mikhail.Shchegolev@rtits.ru>
2020-11-20 01:37:34 +0300

    32: add repo

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be used, if set, instead)."
+SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be used, if set, instead)."
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- hello/views.py
+++ hello/views.py
@@ -18,3 +18,4 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, "db.html", {"greetings": greetings})
+
