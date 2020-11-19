773cbe922086e9f28f62ccfd4f792dd9c41f5de6
Щеголев Михаил Владимирович <Mikhail.Shchegolev@rtits.ru>
2020-11-20 01:41:25 +0300

    32

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.getenv('SECRET_KEY', 'change me!')
 # SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be used, if set, instead)."
 
 # SECURITY WARNING: don't run with debug turned on in production!
